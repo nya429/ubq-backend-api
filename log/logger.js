82ef5ae81f6669752f36de3c28f1fc0ca5da7023
greenchoicenow <song1991yue@gmail.com>
2018-02-28 00:58:14 -0500

    change log dir

--- log/logger.js
+++ log/logger.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const morgan = require('morgan');
 
-const accessLogStream = fs.createWriteStream('./access.log', {flags: 'a',  encoding:'utf8'});
+const accessLogStream = fs.createWriteStream('./log/access.log', {flags: 'a',  encoding:'utf8'});
 const logger = morgan('combined', {stream: accessLogStream});
 
 module.exports = logger;
