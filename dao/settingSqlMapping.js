d239836272d2af59c53132948c735354795e7e7c
yue <yue@yues-MacBook-Pro.local>
2018-09-23 11:44:48 -0400

    change update setting statement by exclude key updatting

--- dao/settingSqlMapping.js
+++ dao/settingSqlMapping.js
@@ -1,6 +1,6 @@
 const setting = {
   insert:'insert into setting_universal(setting_key, setting_value) VALUES(?,?)',
-  update:'update setting_universal set setting_key=?, setting_value=? where setting_id=?',
+  update:'update setting_universal set setting_value=? where setting_id=?',
   delete: 'delete from setting_universal where setting_id=?',
   queryAll: "select * from setting_universal limit ? offset ?",
   queryAllCnt: 'select count(*) from setting_universal',
