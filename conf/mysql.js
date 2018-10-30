0d8ab87fa90e4a823309643bf47f03445e8d3e3c
yue <yue@yues-MacBook-Pro.local>
2018-10-30 10:55:28 -0400

    trim

--- conf/mysql.js
+++ conf/mysql.js
@@ -1,13 +1,13 @@
 /*jslint esversion:6 */
 const mysql = require('mysql');
-//mac local
-// const conf = {mysql: {
-//   connectionLimit : 10,
-//   host     : 'localhost',
-//   user     : 'root',
-//   database : 'ubq_ems_db',
-//   password : 'bsg'
-// }};
+// mac local
+const conf = {mysql: {
+  connectionLimit : 10,
+  host     : 'localhost',
+  user     : 'root',
+  database : 'ubq_ems_db',
+  password : 'bsg'
+}};
 
 // win local
 // const conf = {mysql: {
@@ -19,12 +19,12 @@ const mysql = require('mysql');
 // }};
 
 //aws_rds
-const conf = {mysql: {
-  connectionLimit : 10,
-  host     : 'ubq-ems-db.cpzqoyxyhoka.us-east-2.rds.amazonaws.com',
-  user     : 'root',
-  database : 'ubq_ems_db',
-  password : '88888888'
-}};
+// const conf = {mysql: {
+//   connectionLimit : 10,
+//   host     : 'ubq-ems-db.cpzqoyxyhoka.us-east-2.rds.amazonaws.com',
+//   user     : 'root',
+//   database : 'ubq_ems_db',
+//   password : '88888888'
+// }};
 
 module.exports = conf;
